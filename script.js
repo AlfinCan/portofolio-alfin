ec6f757c56e42585f0081221f8267b1ea0b7f198
alfin <alfinalfinihsan27@gmail.com>
2024-02-15 20:43:13 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    document.getElementById("navBar").classList.toggle("nav");
+  }
+
+function redirInstagram() {
+    window.location.href = "https://instagram.com/alfinihsan_"
+}
+
+function redirWhatsapp() {
+    window.location.href = "https://wa.me/+6285641995098"
+}
+
