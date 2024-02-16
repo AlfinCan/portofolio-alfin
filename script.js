be6ec621d21ec06034643112569f8d2d1e89f147
alfin <alfinalfinihsan27@gmail.com>
2024-02-16 09:30:58 +0700

    update terbaru

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+// Get the button, and when the user clicks on it, execute myFunction
+document.getElementById("menu-icon").onclick = function() {myFunction()};
+
+/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
 function myFunction() {
-    document.getElementById("navBar").classList.toggle("nav");
-  }
+  document.getElementById("myDropdown").classList.toggle("show");
+}
 
 function redirInstagram() {
     window.location.href = "https://instagram.com/alfinihsan_"
